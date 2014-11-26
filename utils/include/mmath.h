7155429ff5a371c3d7c9a9ed674b6f1a44e228d9
LFL <httpftpli@hotmail.com>
2014-11-26 16:37:47 +0800

    fix lcd bug

--- utils/include/mmath.h
+++ utils/include/mmath.h
@@ -7,8 +7,8 @@
 #define MAX(A,B)  (((A)>(B))?(A):(B))
 
 #define ABS(A)    (((A)>0 )?(A):(-(A)))
-#define BOUND(A,B)  ((A)+(B)-1)/(B)*(B) 
-#define DIVUP(A,B)  ((A)+(B)-1)/(B)
+#define BOUND(A,B)  (((A)+(B)-1)/(B)*(B)) 
+#define DIVUP(A,B)  (((A)+(B)-1)/(B))
 
 #define NARRAY(array) (sizeof((array))/sizeof((array[0])))
 #define lenthof(array) NARRAY(array)
